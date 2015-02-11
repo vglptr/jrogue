aa265cc17bfc5d7afe994a84ec608fba9a50197c
hthomy <hthomy@gmail.com>
2015-02-11 13:16:49 +0100

    Missed a file

--- src/util/VertexGenerator.java
+++ src/util/VertexGenerator.java
@@ -6,15 +6,21 @@ import org.lwjgl.BufferUtils;
 
 public class VertexGenerator {
 	public static FloatBuffer generateMesh(int x, int y, float squareSize) {
-		FloatBuffer vertices = BufferUtils.createFloatBuffer(18 * x * y);
+		FloatBuffer vertices = BufferUtils.createFloatBuffer(36 * x * y);
 		for(int i = 0; i < x; i++) {
 			for (int j = 0; j < y; j++) {
-				vertices.put(i).put(j).put(0);
-				vertices.put(i + squareSize).put(j).put(0);
-				vertices.put(i).put(j + squareSize).put(0);
-				vertices.put(i).put(j + squareSize).put(0);
-				vertices.put(i + squareSize).put(j).put(0);
-				vertices.put(i + squareSize).put(j + squareSize).put(0);
+				vertices.put(i*squareSize).put(j*squareSize).put(0);
+				vertices.put(1).put(0).put(0);
+				vertices.put(i*squareSize + squareSize).put(j*squareSize).put(0);
+				vertices.put(0).put(1).put(0);
+				vertices.put(i*squareSize).put(j*squareSize + squareSize).put(0);
+				vertices.put(0).put(0).put(1);
+				vertices.put(i*squareSize).put(j*squareSize + squareSize).put(0);
+				vertices.put(0).put(0).put(1);
+				vertices.put(i*squareSize + squareSize).put(j*squareSize).put(0);
+				vertices.put(0).put(1).put(0);
+				vertices.put(i*squareSize + squareSize).put(j*squareSize + squareSize).put(0);
+				vertices.put(1).put(0).put(0);
 			}
 		}
 		vertices.flip();
